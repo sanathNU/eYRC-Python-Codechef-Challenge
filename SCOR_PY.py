f5344b854f18ca3ba467f9009ef28d8f564eada3
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-10-15 12:32:07 +0530

    last but 1
    finally i'm gettin somewhere

---
+++ SCOR_PY.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 11:46:48 2021
+
+@author: FranticUser
+"""
+
+'''Created for CodeChef Problem SCOR_PY
+Problem Definition:
+    There are N students in the class with distinct names. You are given a list of students and their scores.
+
+    Your task is to find the students with maximum score and print his/her name. If there is tie, print all the names in ascending alphabetical order
+'''
+'''
+Sample Input
+1
+5
+Sam 40.08
+Riya 30.7
+Harry 41
+Anne 35.2
+Peter 36.6
+
+Sample Output
+Harry
+'''
+fin =[]
+for i in range(int(input())):
+    ls ={}
+    n = int(input())
+    for a in range(n):
+        name,score = input().split()
+        ls[name] =score
+        ls1 = sorted(ls.items(),key=lambda kv:(kv[0],kv[1]))
+    finn ={k:v for (k,v) in ls1}
+    
+    for k,v in finn.items():
+        if max(finn.values())==v:
+            print(k)
