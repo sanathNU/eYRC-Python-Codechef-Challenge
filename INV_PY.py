82102576e298b879dc263466519f0d076f1d03da
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-10-15 12:09:18 +0530

    more files
    only 2 more left, the test cases are included as comments

---
+++ INV_PY.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 10:51:00 2021
+
+@author: FranticUser
+"""
+
+'''Created for CodeChef Problem INV_PY
+Problem Definition
+    Ash is tasked with maintaining an inventory list of all the items in a lab. The inventory list contains entries of all the items present in the lab along with its quantity. On daily basis items can be borrowed from the lab or new items can be added to the Lab. Hence Ash usually performs 2 operations on the inventory list:
+    
+    Adding items to the list
+    Deleting items from the list
+    Ash usually does this manually and would prefer to use a program to maintain the inventory list. Your task is to write a program that will help Ash.
+    
+    Consider there are N items in the lab initially. Therefore create a list L that will contain all the names of items (item_name) along with its quantity (item_quantity).
+    
+    Also, make provision for adding/deleting items to/from the list. The item name and its quantity along with the operation (ADD / DELETE) will be specified. You must take care of the following conditions while performing any of the operations on the list.
+'''
+'''
+Sample Input
+1
+3
+Servo 3
+Drone 6
+Board 7
+3
+ADD LEDs 4
+DELETE Board 4
+DELETE Servo 4
+
+Sample Output
+ADDED Item LEDs
+DELETED Item Board
+Item Servo could not be DELETED
+Total Items in Inventory: 16
+'''
+n = int(input())
+for i in range(n):
+    nOfItems = int(input())
+    invent ={}
+    
+    for a in range(nOfItems):
+        item,count = input().split()
+        invent[item] =int(count)
+        
+    nOfOps = int(input())
+    lsOfOperations = [input().split() for b in range(nOfOps)]
+
+    for items in  lsOfOperations:
+        op,name,number = items
+        
+        if op=='ADD':
+            if name in invent.keys():
+                invent[name]+=int(number)
+                print('UPDATED Item',name)
+            else:
+                invent[name]=int(number)
+                print('ADDED Item',name)
+        
+        if op=='DELETE':
+            if name in invent.keys():
+                if int(number) < invent[name]:
+                    invent[name]-=int(number)
+                    print('DELETED Item',name)
+                else:
+                    print(name,'could not be DELETED')  
+            else:
+                print('Item',name,'does not exist')
+    
+    print('Total Items in Inventory:',sum(invent.values()))
+    
---
+++ SLICE1_PY.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 18:15:50 2021
+
+@author: FranticUser
+"""
+
+'''Created for CodeChef Problem SLICE1_PY
+Problem Definition
+Given a list L of numbers and its length, your task is to read these numbers into an array or a list.
+
+Assuming the index starts from 0, use the slice operations to do the following operations:
+
+print list in reverse order
+for every third number starting with index 0, add 3 to it and print them
+for every fifth number starting with index 0, subtract 7 from it and print
+add all the numbers whose index is in between 3 and 7 (inclusive) and print the result
+'''
+'''
+Sample Input
+1
+8
+-2 3 -5 1 8 -4 2 7
+
+Sample Output
+7 2 -4 8 1 -5 3 -2 4 5
+-11
+14
+'''
+
+nOfTest = int(input())
+for i in range(nOfTest):
+    aLen = int(input())
+    mainList = list(map(int,input().split()))
+    revList = mainList[::-1]
+    Index3 = [mainList[x]+3 for x in range(1,aLen) if x%3==0]
+    Index5 = [mainList[x]-7 for x in range(1,aLen) if x%5==0]
+    FinalSum = sum(mainList[3:8])
+    
+    print(' '.join(map(str,revList)),' '.join(map(str,Index3)))
+    # print(' '.join(map(str,Index3)))
+    print(' '.join(map(str,Index5)))
+    print(FinalSum)
+    
