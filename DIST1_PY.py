4e9f06ee9e0993fb470d8ba602904e39e968eb36
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-10-16 09:21:51 +0530

    huge changes
    lots of comments...had to change the entire structure of code

--- DIST1_PY.py
+++ DIST1_PY.py
@@ -15,12 +15,40 @@ Problem Statement:
 
 The distance computed should be precise up to 2 decimal places.
 '''
-ls = []
-for i in range(int(input())):
-    x1,y1,x2,y2 = map(int,input().split())
-    dist = float(pow(pow(x2-x1,2)+pow(y2-y1,2),0.5))
-    ls.append(dist)
+'''
+Sample Input
+2
+-1 0 5 0
+2 1 6 -2
 
-for items in ls:
-    print('Distance: %.2f'%items)
-    
+Sample Output
+Distance: 6.00
+Distance: 5.00
+'''
+#This is written according to the format given by codechef
+# Import any required module/s
+import math;
+
+# Function to calculate Euclidean distance between two points
+def compute_distance(x1, y1, x2, y2):
+
+    distance = 0
+    distance=math.sqrt(((y2-y1)**2)+((x2-x1)**2))
+    # Complete this function to return Euclidean distance and
+    # print the distance value with precision up to 2 decimal places
+    print("Distance: "+"{0:.2f}".format(distance))
+
+# Main function
+if __name__ == '__main__':
+
+    # Take the T (test_cases) input
+    test_cases = int(input())
+
+    # Write the code here to take the x1, y1, x2 and y2 values
+    test=list()
+    for _ in range(test_cases):
+        test.append(list(map(int,input().split())))
+        # Once you have all 4 values, call the compute_distance function to find Euclidean distance
+    for cases in test:
+        x1,y1,x2,y2=cases
+        compute_distance(x1, y1, x2, y2)
