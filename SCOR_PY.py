1caa97b8fdb3d8a6679ba1ceab42cb567358a0e2
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-10-16 09:32:00 +0530

    added documentation

--- SCOR_PY.py
+++ SCOR_PY.py
@@ -24,16 +24,20 @@ Peter 36.6
 Sample Output
 Harry
 '''
+#temporary list for storing stuff
 fin =[]
 for i in range(int(input())):
+    # a dictionary for each testcase
     ls ={}
     n = int(input())
     for a in range(n):
         name,score = input().split()
         ls[name] =score
+        #sorting the dictionary on basis of key and storing it as a list
         ls1 = sorted(ls.items(),key=lambda kv:(kv[0],kv[1]))
+    # Storing it as a new sorted dictionary
     finn ={k:v for (k,v) in ls1}
-    
+    # logic for finding the key in a dictionary when given a value
     for k,v in finn.items():
         if max(finn.values())==v:
             print(k)
