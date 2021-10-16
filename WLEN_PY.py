2df940bd46d5bf7de96515cfdad19d407c410bbd
Sanath <77428228+sanathNU@users.noreply.github.com>
2021-10-16 09:26:25 +0530

    added testcases
    And also some comments to make the core more readable

--- WLEN_PY.py
+++ WLEN_PY.py
@@ -13,8 +13,21 @@ Problem Definition:
 
     Each string str will begin with "@".
 '''
+'''
+Sample Input
+2
+@Debugging is being the detective in a criminal movie where you are also the murderer
+@Good code is its own best documentation
+
+Sample Output
+9,2,5,3,9,2,1,8,5,5,3,3,4,3,8
+4,4,2,3,3,4,13
+'''
+#list comprehension for storing all input strings as a list of words
 ls =[list(map(str,input().split())) for i in range(int(input()))]
 for items in ls:
+    #removing the '@' from the beginning of each word of testcase
     items[0]=items[0][1:]
+    #finding the length of each word and printing it
     a =[len(x) for x in items]
     print(','.join(map(str,a)))
